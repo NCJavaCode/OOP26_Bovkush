f2d78fcb2427e349899d3ef88fa9f9b515382dc0
Usern <bracto0@gmail.com>
2026-09-10 12:12:29 +0300

    створив класс Book

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year > 0 && year <= 2026) {
+            this.year = year;
+        }
+    }
+}
